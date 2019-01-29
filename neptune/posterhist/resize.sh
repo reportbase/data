157cd9009ff01034487e4a99567f54f651969573
Tom Brinkman <tom@repba.com>
2019-01-28 18:41:17 -0600

    na

--- neptune/posterhist/resize.sh
+++ neptune/posterhist/resize.sh
@@ -1,2 +1,1 @@
-#for image in *.jpg; do convert $image -resize 40% -quality 70 small/$image; done
-for image in *.jpg; do convert $image -resize 20% -quality 50 tiny/$image; done
+for image in *.jpg; do convert $image -resize 50% -quality 70 tiny/$image; done
--- neptune/posters/resize.sh
+++ neptune/posters/resize.sh
@@ -1,2 +1,2 @@
-for image in *.jpg; do convert $image -resize 40% -quality 70 small/$image; done
+for image in *.jpg; do convert $image -resize 50% -quality 70 small/$image; done
 #for image in *.jpg; do convert $image -resize 20% -quality 50 tiny/$image; done
