b636c02dc42ad826c07356877e9e72289af397bf
Tom Brinkman <tom@repba.com>
2019-09-14 04:55:33 -0600

    na

--- imgs/CARD/run.sh
+++ imgs/CARD/run.sh
@@ -5,31 +5,35 @@
 #  let a=a+1
 #done
 
-mogrify -size 320 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
-mkdir 320 2> tmp
-mv *.png 320
+mogrify -size 240 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
+mkdir 240 2> tmp
+mv *.png 240
 
-mogrify -size 640 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
-mkdir 640 2> tmp
-mv *.png 640
+#mogrify -size 320 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
+#mkdir 320 2> tmp
+#mv *.png 320
 
-mogrify -size 960 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
-mkdir 960 2> tmp
-mv *.png 960
+#mogrify -size 640 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
+#mkdir 640 2> tmp
+#mv *.png 640
 
-mogrify -size 1280 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
-mkdir 1280 2> tmp
-mv *.png 1280
+#mogrify -size 960 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
+#mkdir 960 2> tmp
+#mv *.png 960
 
-mogrify -size 1280 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
-mkdir 1280 2> tmp
-mv *.png 1280
+#mogrify -size 1280 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
+#mkdir 1280 2> tmp
+#mv *.png 1280
 
-mogrify -size 1600 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
-mkdir 1600 2> tmp
-mv *.png 1600
+#mogrify -size 1280 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
+#mkdir 1280 2> tmp
+#mv *.png 1280
 
-mogrify -size 1920 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
-mkdir 1920 2> tmp
-mv *.png 1920
+#mogrify -size 1600 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
+#mkdir 1600 2> tmp
+#mv *.png 1600
+
+#mogrify -size 1920 -channel rgba -background "rgba(0,0,0,0)" -format png *.svg
+#mkdir 1920 2> tmp
+#mv *.png 1920
 
