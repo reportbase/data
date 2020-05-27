c29176b62f2370ae96b1f50e4a36ef2c2003cdd2
Tom Brinkman <tom@repba.com>
2020-05-27 17:29:52 -0600

    na

--- bin/resize.sh
+++ bin/resize.sh
@@ -1,4 +1,4 @@
-for image in 0*.jpg; do convert $image -resize x1080 -quality 75 $image; done
+for image in 4*.jpg; do convert $image -sampling-factor 4:2:0 -strip -quality 65 -interlace JPEG -colorspace RGB $image ; done
 
 
 
