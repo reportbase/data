e67da7a23a1944f54963a1fdebad0d9f7c0c08d1
Tom Brinkman <tom@repba.com>
2020-08-08 02:29:35 -0600

    na

---
+++ boss/0097/imga/orig/sort.js
@@ -0,0 +1,4 @@
+#load sort.ini
+#create array of names
+#output each name to "mv old new"
+#copy pages.ini parent folder
--- boss/findsh.sh
+++ boss/findsh.sh
@@ -1,1 +1,1 @@
- find $directory -type f -name "*.sh"
+ find $directory -type f -name "*resize*.sh"
