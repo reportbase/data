a025d06116c65dce22ebb33cf239fae6cb1e5b79
Tom Brinkman <tom@repba.com>
2020-09-06 17:12:45 -0600

    na

--- boss/0001/old/imgc/2x1.sh
+++
@@ -1,4 +0,0 @@
-montage a.jpg b.jpg -tile 2x1 -mode Concatenate '0014.jpg' 2> error
-
-
-
--- boss/0001/old/resize.sh
+++
@@ -1,4 +0,0 @@
-for image in 0*.jpg; do convert $image -sampling-factor 4:2:0 -strip -quality 80 -interlace JPEG -colorspace RGB $image ; done
-
-
-
