838b897b581fd9c8091464a6b43e41f070467809
Tom Brinkman <tom@repba.com>
2020-09-07 10:32:10 -0600

    na

---
+++ boss/1728x1080/move.sh
@@ -0,0 +1,4 @@
+﻿mv cyber.city tank.combat space.combat party gamer naval.combat merlin little.red ../1920x1080
+mv infantry.combat anime.groups first.person face cyber.tech cyber.face arthur air.combat ../1920x1080
+mv cyber.city animals airplanes sports food models racing combat comics cartoons spiderman ../1920x1080
+mv ocean exercise concepts tiny effects assorted ../1920x1080
---
+++ boss/2560x1080/assorted/orig/resize.sh
@@ -0,0 +1,1 @@
+for image in *.jpg; do convert $image -sampling-factor 4:2:0 -strip -quality 80 -resize x1080 -interlace JPEG -colorspace RGB $image ; done
